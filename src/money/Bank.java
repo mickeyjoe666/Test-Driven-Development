f218f2ccdf677f04a0d85a51be16c07240d2d4cb
mickeyjoe666 <mickeyjoe666@gmail.com>
2020-10-17 13:27:39 +0900

    第15章 テスト任せとコンパイラ任せ

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -9,11 +9,11 @@ public class Bank {
         return source.reduce(this, to);
     }
 
-    void addRate (String from, String to, int rate) {
+    void addRate(String from, String to, int rate) {
         rates.put(new Pair(from, to), rate);
     }
 
-    int rate (String from, String to) {
+    int rate(String from, String to) {
         if (from.equals(to)) return 1;
         return rates.get(new Pair(from, to));
     }
